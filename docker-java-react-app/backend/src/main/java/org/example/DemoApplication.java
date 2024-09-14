ac1ecbaf12830f29caf262556fa1ae4124c68b15
Juan David <juan.david.garcia.pulido@gmail.com>
2024-09-13 21:35:19 -0500

    expo

---
+++ docker-java-react-app/backend/src/main/java/org/example/DemoApplication.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/api/message")
+    public String getMessage() {
+        return "Hello from the backend!";
+    }
+}
---
+++ docker-java-react-app/frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css'; // Asegúrate de que el archivo CSS esté enlazado
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Docker-Java-React-App</h1>
+                <h2>ARSW</h2>
+            </header>
+        </div>
+    );
+}
+
+export default App;
